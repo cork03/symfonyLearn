5de3152898af35fc0acd42549c0f0d758cf7c598
住吉翼 <tsubasa.t1119@gmail.com>
2022-07-31 13:38:48 +0900

    twig sample

--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -2,22 +2,23 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
     /**
      * @Route("/lucky/number")
      *
-     * @return void
+     * @return Response
      */
-    public function number()
+    public function number() : Response
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
