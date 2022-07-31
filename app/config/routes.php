d6d1f9181d7e341c37785ea984fdc0b5c4bd317a
住吉翼 <tsubasa.t1119@gmail.com>
2022-07-31 18:49:00 +0900

    json test

--- app/config/routes.php
+++ app/config/routes.php
@@ -5,11 +5,9 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
     $routes->add('lucky_number', '/lucky/number')
-        // the controller value has the format [controller_class, method_name]
         ->controller([LuckyController::class, 'number'])
-
-        // if the action is implemented as the __invoke() method of the
-        // controller class, you can skip the 'method_name' part:
-        // ->controller(BlogController::class)
+    ;
+    $routes->add('lucky_number', '/lucky/json')
+        ->controller([LuckyController::class, 'testJson'])
     ;
 };
--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController extends AbstractController
@@ -14,9 +15,21 @@ class LuckyController extends AbstractController
     public function number() : Response
     {
         $number = random_int(0, 100);
+        $generatedUrl = $this->generateUrl('lucky_number');
 
         return $this->render('lucky/number.html.twig', [
             'number' => $number,
+            'url' => $generatedUrl,
         ]);
+    }
+
+    /**
+     * json test
+     *
+     * @return JsonResponse
+     */
+    public function testJson() : JsonResponse
+    {
+        return $this->json(['test' => 'test']);
     }
 }
