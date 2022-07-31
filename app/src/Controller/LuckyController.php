6aaed884465b243d2118e7b76850da90b757b4d7
住吉翼 <tsubasa.t1119@gmail.com>
2022-07-31 18:23:58 +0900

    routing fix from annotation to route.php

--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -4,12 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
     /**
-     * @Route("/lucky/number")
      *
      * @return Response
      */
